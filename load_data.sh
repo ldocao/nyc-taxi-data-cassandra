087ed2b3e98aab1fd901e0d3a0dbe9590aa5e690
Long Do Cao <long@homingbird.ai>
2021-09-21 18:20:02 +0200

    fix: add .cql

--- load_data.sh
+++ load_data.sh
@@ -2,16 +2,16 @@
 
 cqlsh -f $HOME/nyc-taxi-data-cassandra/create_tables.cql
 
-data=`ls -1 $HOME/nyc-taxi-data/denormalized/`
+data=`ls -1 $HOME/nyc-taxi-data/denormalized/*.csv.gz`
 N=`grep -c ^processor /proc/cpuinfo`
 
 for f in $data
 do
     ((i=i%N)); ((i++==0)) && wait
-    gunzip -c $HOME/nyc-taxi-data/denormalized/$f > $f.unzip #this is totally not a good filename but we will delete it anyway
-    cat $HOME/nyc-taxi-data-cassandra/load_trip_information.cql | sed -e "s%{{ filename }}%$HOME/nyc-taxi-data/denormalized/$f.unzip%"  > load_trip_information_$f.cql
-    cqlsh -f load_trip_information_$f.cql
-    rm load_trip_information_$f.cql
+    gunzip -c $f > $f.unzip #this is totally not a good filename but we will delete it anyway
+    cat $HOME/nyc-taxi-data-cassandra/load_trip_information.cql | sed -e "s%{{ filename }}%$f.unzip%"  > $f.cql
+    cqlsh -f $f.cql
+    rm $f.cql
     rm $f.unzip
 done
 
