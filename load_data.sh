8f6bbb15b2315a61a62dc2a6ffbcf595c1774615
Long Do Cao <long@homingbird.ai>
2021-09-21 18:15:21 +0200

    fix: use % in sed because replaced string contains /

--- load_data.sh
+++ load_data.sh
@@ -8,8 +8,8 @@ N=`grep -c ^processor /proc/cpuinfo`
 for f in $data
 do
     ((i=i%N)); ((i++==0)) && wait
-    gunzip $HOME/nyc-taxi-data/denormalized/$f -c > $f.unzip #this is totally not a good filename but we will delete it anyway
-    cat $HOME/nyc-taxi-data-cassandra/load_trip_information.cql | sed "s/{{ filename }}/$HOME/nyc-taxi-data/denormalized/$f.unzip/"  > load_trip_information_$f.cql
+    gunzip -c $HOME/nyc-taxi-data/denormalized/$f > $f.unzip #this is totally not a good filename but we will delete it anyway
+    cat $HOME/nyc-taxi-data-cassandra/load_trip_information.cql | sed -e "s%{{ filename }}%$HOME/nyc-taxi-data/denormalized/$f.unzip%"  > load_trip_information_$f.cql
     cqlsh -f load_trip_information_$f.cql
     rm load_trip_information_$f.cql
     rm $f.unzip
