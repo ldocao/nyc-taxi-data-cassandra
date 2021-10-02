c3a73ee69682e68be2e21aac5e21e6b9bc0b5faa
Long Do Cao <long@homingbird.ai>
2021-10-02 14:35:57 +0200

    fix: stable output

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install -y cassandra
 
 #tools to bulk load csv
-wget https://github.com/brianmhess/cassandra-loader/releases/download/v0.0.27/cassandra-loader # https://github.com/brianmhess/cassandra-loader
+wget https://github.com/brianmhess/cassandra-loader/releases/download/v0.0.27/cassandra-loader -O $HOME/cassandra-loader # https://github.com/brianmhess/cassandra-loader
 chmod u+x $HOME/cassandra-loader
 sudo mv $HOME/cassandra-loader /usr/local/bin
 
