d302c92fd2e420b5533ba1e09bf4ac17f8cccacd
Long Do Cao <long@homingbird.ai>
2021-10-02 14:41:49 +0200

    fix: cassandra loader

--- load_data.sh
+++ load_data.sh
@@ -8,7 +8,7 @@ for f in $data
 do
     #load the trip_information table
     gunzip -c $f > $f.unzip #this is totally not a good filename but we will delete it anyway
-    ./cassandra-loader -f $f.unzip -host 127.0.0.1 -schema "trip.trip_information(trip_id,vendor_id,pickup_datetime,dropoff_datetime,store_and_fwd_flag,rate_code_id,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count,trip_distance,fare_amount,extra,mta_tax,tip_amount,tolls_amount,ehail_fee,improvement_surcharge,total_amount,payment_type,trip_type,precipitation,snow_depth,snowfall,max_temperature,min_temperature,average_wind_speed)" -dateFormat "yyy-MM-dd HH:mm:ss"
+    cassandra-loader -f $f.unzip -host 127.0.0.1 -schema "trip.trip_information(trip_id,vendor_id,pickup_datetime,dropoff_datetime,store_and_fwd_flag,rate_code_id,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count,trip_distance,fare_amount,extra,mta_tax,tip_amount,tolls_amount,ehail_fee,improvement_surcharge,total_amount,payment_type,trip_type,precipitation,snow_depth,snowfall,max_temperature,min_temperature,average_wind_speed)" -dateFormat "yyy-MM-dd HH:mm:ss"
     rm $f.unzip
 
     #load the other tables
